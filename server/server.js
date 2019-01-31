be061485f6d6f15775c2b5393040b958ddce8004
Rasmussen <Karl.Rasmussen@henryschein.com>
2019-01-31 14:46:58 -0700

    Add cpmmect amd doscpmmect evemt jamd;ers

--- server/server.js
+++ server/server.js
@@ -1,19 +1,30 @@
 require('./config/config');
 
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT;
 
 var app = express();
-
-const port = process.env.PORT;
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log('New user connected');
+  
+    socket.on('disconnect', () => {
+      console.log('User was disconnected');
+    });
+  });
+
+server.listen(port, () => {
     console.log(`Started up at port ${port}`);
 });
   
-module.exports = {app};
+//module.exports = {app};
   
