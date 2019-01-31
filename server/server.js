6fe5ab6d1e02e7cdd58a0ab2fc21aa85feedcdb6
Rasmussen <Karl.Rasmussen@henryschein.com>
2019-01-31 12:15:28 -0700

    new chat-app

---
+++ server/server.js
@@ -0,0 +1,19 @@
+require('./config/config');
+
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+
+var app = express();
+
+const port = process.env.PORT;
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log(`Started up at port ${port}`);
+});
+  
+module.exports = {app};
+  
